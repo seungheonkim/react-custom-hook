d7fa2e57b7c4df9539a901d85880ea21aecbe463
seungheonkim <seungheon818@naver.com>
2022-11-28 11:19:49 +0900

    useEffect-custom-hook

---
+++ src/util/useFetch.js
@@ -0,0 +1,29 @@
+//app.js에서 함수를 가져와서 Custom hook을 만들어 봅시다.
+//hooks.js의 이름도 알맞게 바꿔주세요.
+import {useEffect, useState} from "react";
+
+const useFetch = (fetchUrl) => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch("data.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            }
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                setData(myJson);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [fetchUrl]);
+
+    return data;
+};
+
+export default useFetch;
